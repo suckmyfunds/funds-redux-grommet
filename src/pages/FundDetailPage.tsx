ff8396cdd5b8afacd36f968750dfd12c9a429ffb
Intey <ziexe0@gmail.com>
2024-03-19 18:30:31 +0300

    error boundary build

--- src/pages/FundDetailPage.tsx
+++ src/pages/FundDetailPage.tsx
@@ -12,7 +12,7 @@ class ErrorBoundary extends React.Component<PropsWithChildren, { hasError: boole
     this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error: any) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
